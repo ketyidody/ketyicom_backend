f860e5a0729fbe7544f67854e589726122fe9e59
jozefketyi <ketyi.dody@gmail.com>
2024-11-20 16:57:02 +0100

    ketyi.com backend - add thumbnail image to folders as the first image from their gallery

--- src/Controller/Api/FolderController.php
+++ src/Controller/Api/FolderController.php
@@ -3,25 +3,33 @@
 namespace App\Controller\Api;
 
 use App\Entity\EasyMedia\Folder;
+use App\Entity\EasyMedia\Media;
 use Doctrine\ORM\EntityManagerInterface;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FolderController extends AbstractController
 {
     #[Route('/api/folders', methods: ['GET'])]
-    public function getFolders(EntityManagerInterface $entityManager)
+    public function getFolders(EntityManagerInterface $entityManager, CacheManager $cacheManager): Response
     {
         $folderRepository = $entityManager->getRepository(Folder::class);
+        $mediaRepository = $entityManager->getRepository(Media::class);
         $folders = $folderRepository->findAll();
 
         return new JsonResponse([
             'success' => true,
             'items' => collect($folders)->map(fn (Folder $folder) => [
-                'id' => $folder->getId(), 
-                'slug' => $folder->getSlug(), 
-                'name' => $folder->getName()
+                'id' => $folder->getId(),
+                'slug' => $folder->getSlug(),
+                'name' => $folder->getName(),
+                'folderThumbnailPath' => $cacheManager->getBrowserPath(
+                    $mediaRepository->findOneBy(['folder' => $folder])->getPath(),
+                    'thumbnail'
+                ),
             ])
         ]);
     }
