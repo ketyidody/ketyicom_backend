9308357d4e9e915d1f7e139c22eb09c78b417a0c
jozefketyi <ketyi.dody@gmail.com>
2024-12-16 20:53:19 +0100

    ketyi.com backend - migration added

---
+++ migrations/Version20241212223241.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241212223241 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE order_items (order_id INT NOT NULL, product_id INT NOT NULL, INDEX IDX_62809DB08D9F6D38 (order_id), INDEX IDX_62809DB04584665A (product_id), PRIMARY KEY(order_id, product_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE order_items ADD CONSTRAINT FK_62809DB08D9F6D38 FOREIGN KEY (order_id) REFERENCES `order` (id)');
+        $this->addSql('ALTER TABLE order_items ADD CONSTRAINT FK_62809DB04584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE `order` DROP order_items');
+        $this->addSql('ALTER TABLE product CHANGE price price DOUBLE PRECISION NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE order_items DROP FOREIGN KEY FK_62809DB08D9F6D38');
+        $this->addSql('ALTER TABLE order_items DROP FOREIGN KEY FK_62809DB04584665A');
+        $this->addSql('DROP TABLE order_items');
+        $this->addSql('ALTER TABLE product CHANGE price price NUMERIC(10, 0) NOT NULL');
+        $this->addSql('ALTER TABLE `order` ADD order_items JSON NOT NULL COMMENT \'(DC2Type:json)\'');
+    }
+}
